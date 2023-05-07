2fc8b705ce047f58e4a412e3882e4c599d946f38
denis.topallaj <denis.topallaj@student.howest.be>
2023-05-07 21:16:22 +0200

    add: css to make page look good, also responsive when wanting to print it, toolbar options added

--- docs-client/src/components/TextEditor.tsx
+++ docs-client/src/components/TextEditor.tsx
@@ -2,6 +2,24 @@ import "quill/dist/quill.snow.css";
 import Quill from "quill";
 import { useCallback } from "react";
 
+const TOOLBAR_OPTIONS = [
+	["bold", "italic", "underline", "strike"],
+	["image", "blockquote", "code-block"],
+
+	[{ list: "ordered" }, { list: "bullet" }],
+	[{ script: "sub" }, { script: "super" }],
+	[{ indent: "-1" }, { indent: "+1" }],
+	[{ direction: "rtl" }],
+
+	[{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+	[{ color: [] }, { background: [] }],
+	[{ font: [] }],
+	[{ align: [] }],
+
+	["clean"],
+];
+
 function TextEditor() {
 	const wrapperRef = useCallback((wrapper: HTMLDivElement) => {
 		if (!wrapper) return;
@@ -9,10 +27,13 @@ function TextEditor() {
 		wrapper.innerHTML = "";
 		const editor = document.createElement("div");
 		wrapper.append(editor);
-		new Quill(editor, { theme: "snow" });
+		new Quill(editor, {
+			theme: "snow",
+			modules: { toolbar: TOOLBAR_OPTIONS },
+		});
 	}, []);
 
-	return <div id="container" ref={wrapperRef}></div>;
+	return <div className="container" ref={wrapperRef}></div>;
 }
 
 export default TextEditor;
